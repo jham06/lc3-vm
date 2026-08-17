dc3fcb584612733be0ccebdbbe59ef32f45d4ae3
jham06 <jhamthebeast227@gmail.com>
2026-08-17 17:15:32 -0600

    some cases done. need to fix something

--- main.c
+++ main.c
@@ -46,11 +46,27 @@ uint16_t registers[10]; // Create the register array with 10 total registers.
 
 
 int main (int argc, const char *argv) {// Why do we use a const?
+
+    // Need to set a condition as we expect one or more paths to VM images. 
+
+    if (argc < 2) {
+        printf("lc3 [image-file1] .. \n"); // wtf is this. PINPOINT.
+        exit(2); // exit code 2, missing file. 
+    }
+
+    for (int i = 1; i < argc; i++) { // expect one or more paths to vm image. 
+
+        if (!read_image(argv[i])) {
+            printf("Failed to load image: %s\n", argv[i]);
+            exit(1);
+        }  
+    }
     registers[R_COND] = FL_ZRO; // only one condition should be set at any time, so set Z FLAG. 
 
     registers[R_PC] = 0x3000;
 
     int run_loop = 67;
+
 
     while (run_loop) { 
         uint16_t instr = mem_read(registers[R_PC]);
@@ -59,38 +75,73 @@ int main (int argc, const char *argv) {// Why do we use a const?
 
         uint16_t opcode = instr >> 12;
 
-        switch (opcode) {
+        switch (opcode) {  // instr format: 0000 0000 0000 0000 
     
-            case ADD_OP:
+            case ADD_OP: // 0001
+                // Need to consider two cases: When bit 5 is 0/1. 
+
+                uint16_t bit5 = (instr >> 5) & 0x0001;
+                uint16_t dr = (instr >> 9) & 0x0007; 
+                uint16_t sr1 = (instr >> 6) & 0x0007;
+                if (bit5 == 0) {
+                    uint16_t sr2 = (instr & 0x0007);
+                    registers[dr] = registers[sr1] + registers[sr2];
+                } else {
+                    int16_t imm5 = (instr & 0x001F);
+                    imm5 = imm5 << 11; // sign_extend.
+                    imm5 = imm5 >> 11;
+                    registers[dr] = imm5 + registers[sr1];
+                }
                 break;
-            case AND_OP:
+            case AND_OP: // 0101
+                uint16_t bit5 = (instr >> 5) & 0x0001;
+                uint16_t dr = (instr >> 9) & 0x0007; 
+                uint16_t sr1 = (instr >> 6) & 0x0007;
+                if (bit5 == 0) {
+                    uint16_t sr2 = (instr & 0x0007);
+                    registers[dr] = registers[sr1] & registers[sr2];
+                } else {
+                    int16_t imm5 = (instr & 0x001F);
+                    imm5 = imm5 << 11; // sign_extend.
+                    imm5 = imm5 >> 11;
+                    registers[dr] = imm5 & registers[sr1];
+                }
                 break;
-            case NOT_OP:
+            case NOT_OP: // 1001
+                uint16_t dr = (instr >> 9) & 0x0007; 
+                uint16_t sr = (instr >> 6) & 0x0007;
+
+                registers[dr] = ~(registers[sr]);
                 break;
-            case BR_OP:
+            case BR_OP: // 0000, need to test N, Z and P
+                uint16_t n = (instr >> 11) & 0x0001;
+                uint16_t z = (instr >> 10) & 0x0001;
+                uint16_t p = (instr >> 9) & 0x0001;
+
+                 // LETS come back to this.
                 break;
-            case JMP_OP:
+            case JMP_OP: // 1100
                 break;
-            case JSR_OP:
+            case JSR_OP: // 0100
                 break;
-            case LD_OP:
+            case LD_OP: // 0010
                 break;
-            case LDI_OP:
+            case LDI_OP: // 1010
                 break;
-            case LDR_OP:
+            case LDR_OP: // 0110
                 break;
-            case LEA_OP:
+            case LEA_OP: // 1110
                 break;
-            case ST_OP:
+            case ST_OP: // 0011
                 break;
-            case STI_OP:
+            case STI_OP: // 1011
                 break;
-            case STR_OP:
+            case STR_OP: // 0111
                 break;
-            case TRAP_OP:
+            case TRAP_OP: // 1111
                 break;
-            case RES_OP:
-            case RTI_OP:
+            case RES_OP: // 1101
+            case RTI_OP: // 1000
             default:
                 // Some opcode..
                 break;
